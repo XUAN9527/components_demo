a7e6dfdd1daff78c06211403f5934a70f9c37465
Xuan <Songqx@breo.com>
2024-02-23 16:55:04 +0800

    first commit

---
+++ components/letter_shell/shell_port.c
@@ -0,0 +1,85 @@
+/**
+ * @file shell_port.c
+ * @author Letter (NevermindZZT@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-02-22
+ * 
+ * @copyright (c) 2019 Letter
+ * 
+ */
+
+#include "shell.h"
+#include "freertos/FreeRTOS.h"
+#include "driver/uart.h"
+#include "log.h"
+
+#define     LETTER_SHELL_UART      UART_NUM_1
+
+Shell shell;
+Log uartLog;
+char shellBuffer[512];
+
+void uartLogWrite(char *buffer, short len)
+{
+    if (uartLog.shell)
+    {
+        shellWriteEndLine(uartLog.shell, buffer, len);
+    }
+}
+
+/**
+ * @brief 用户shell写
+ * 
+ * @param data 数据
+ * @param len 数据长度
+ * 
+ * @return unsigned short 写入实际长度
+ */
+unsigned short userShellWrite(char *data, unsigned short len)
+{
+    return uart_write_bytes(LETTER_SHELL_UART, (const char *)data, len);
+}
+
+
+/**
+ * @brief 用户shell读
+ * 
+ * @param data 数据
+ * @param len 数据长度
+ * 
+ * @return unsigned short 读取实际长度
+ */
+signed char userShellRead(char *data, unsigned short len)
+{
+    return uart_read_bytes(LETTER_SHELL_UART, (uint8_t *)data, len, portMAX_DELAY);
+}
+
+
+/**
+ * @brief 用户shell初始化
+ * 
+ */
+void userShellInit(void)
+{
+    uart_config_t uartConfig = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    uart_param_config(LETTER_SHELL_UART, &uartConfig);
+    uart_driver_install(LETTER_SHELL_UART, 256 * 2, 0, 0, NULL, 0);
+    uart_set_pin(LETTER_SHELL_UART, 4, 5, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    shell.write = userShellWrite;
+    shell.read = userShellRead;
+    shellInit(&shell, shellBuffer, 512);
+
+    uartLog.write = uartLogWrite;
+    uartLog.active = true;
+    uartLog.level = LOG_DEBUG;
+    logRegister(&uartLog, &shell);
+
+    xTaskCreate(shellTask, "shell", 2048, &shell, configMAX_PRIORITIES - 1, NULL);
+}
---
+++ main/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+//#include "drv_usart.h"
+//#include "drv_adc.h"
+//#include "drv_gpio.h"
+//#include "drv_ble.h"
+#include "shell_port.h"
+#include "log.h"
+#include "nvs_flash.h"
+
+static const char *TAG = "sample test";
+
+void app_main(void)
+{
+	esp_err_t ret;
+
+    // Initialize NVS.
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+	esp_log_level_set(TAG, ESP_LOG_INFO);
+	ESP_LOGI(TAG,"this is a test program");
+	// mcu_usart1_init();
+    
+	userShellInit();
+	// drv_ble_init();
+
+	logInfo("log info!");
+	logDebug("log debug!");
+	logWarning("log warning!");
+	logError("log error!");
+	while(1)
+	{
+		logDebug("log loop!");
+		vTaskDelay(pdMS_TO_TICKS(2000));
+	}
+}
